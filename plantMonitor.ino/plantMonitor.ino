63fa95de42cf990bb727beb6de1c12dc4d8a2ea2
sirgypsyg <siemagosciu32@gmail.com>
2024-01-20 20:30:29 +0100

    init
    working

---
+++ plantMonitor.ino/plantMonitor.ino
@@ -0,0 +1,27 @@
+#define echoPin A0;
+#define LEDD 13
+long duration;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(LEDD, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  Serial.println( 1023 - analogRead(A0));
+  if(1023 - analogRead(A0) < 300){
+    digitalWrite(LEDD, HIGH);  // turn the LED on (HIGH is the voltage level)
+  }
+  else{
+     digitalWrite(LEDD, LOW);
+  }
+  delay(10);
+}
+
+
+//TESTING
