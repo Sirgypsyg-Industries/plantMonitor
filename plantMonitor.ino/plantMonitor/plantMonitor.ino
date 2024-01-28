7e5f0c758b974d9b7aa3d2c77d104f434ee0100f
Michał M <65555461+Velshart@users.noreply.github.com>
2024-01-28 17:34:45 +0100

    new beginning

--- plantMonitor.ino/plantMonitor/plantMonitor.ino
+++ plantMonitor.ino/plantMonitor/plantMonitor.ino
@@ -1,46 +1,42 @@
-#include <U8g2lib.h>
-#define sensor A0
-#define LED 13
-#define BUZZER 8
+#include "dht.h"
+#define dht_apin A0 // Analog Pin sensor is connected to
+ 
+dht DHT;
+int var;
+ 
+void setup(){
+  //buzzer
+  pinMode(2,OUTPUT);
+  pinMode(A1, INPUT);
 
-
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // font?
-
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(sensor, INPUT);
-  pinMode(LED, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
-  u8g2.begin();
   Serial.begin(9600);
-
-}
-
-void loop() {
-
-   //handle oled
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB14_tr); // font
-  u8g2.drawStr(10, 30, "sample test"); // coordinates of text(10, 30)
-  u8g2.sendBuffer();
-  //
-
-  
-  Serial.println( 1023 - analogRead(sensor));
-  bool criticalReached = 1023 - analogRead(sensor) > 300;
-
-  if(criticalReached) {
-    digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
-
-      digitalWrite(BUZZER, HIGH);
-      delay(1000);
-      digitalWrite(BUZZER, LOW);
-      delay(1000);
-
+   
+}//end "setup()"
+ 
+void loop(){
+  //Start of Program 
+  if(Serial.available()) {
+    var=Serial.read();
+    if(var == 'A') {
+      tone(2, 500);
+    }
+    if(var == 'B') {
+      noTone(2);
+    }
   }
-  else{
-     digitalWrite(LED, LOW);
-  }
-  delay(1000);
-}
+    DHT.read11(dht_apin);
+    Serial.print("humidity = ");
+    Serial.print(DHT.humidity); 
+    Serial.println("% ");
+
+    delay(500);
+   Serial.println( 1023 - analogRead(A1));
+
+   delay(500);
+
+
+    
+    //delay(150);//Wait 5 seconds before accessing sensor again.
+  //Fastest should be once every two seconds.
+ 
+}// end loop()
