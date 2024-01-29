6e2abb908cf5ddb5d446b9073d1fe055e5cf2a7e
sirgypsyg <siemagosciu32@gmail.com>
2024-01-29 19:58:50 +0100

    oled functionality
    •small bugfixes
    •oled display to show parameters

--- plantMonitor.ino/plantMonitor/plantMonitor.ino
+++ plantMonitor.ino/plantMonitor/plantMonitor.ino
@@ -1,32 +1,87 @@
 #include "dht.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
+#include <Adafruit_Sensor.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1
 #define dht_apin A0 // Analog Pin sensor is connected to
- 
+
+Adafruit_SH1106 display(OLED_RESET);
 dht DHT;
 int var;
-int criticalTemp = 1;
+double criticalTemp = 1;
 
 int maxSoilContent = 1;
 int minSoilContent = 1;
 double criticalSoilContent = 1;
 
+void stringDisplay(String s){
 
+  display.clearDisplay();
+
+  // display temperature
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("Temperature: ");
+  display.setTextSize(2);
+  display.setCursor(0,10);
+  display.print(s);
+  display.print(" ");
+  display.setTextSize(1);
+  display.cp437(true);
+  display.write(167);
+  display.setTextSize(2);
+
+  display.display(); 
+}
+
+void parametersDisplay(double t, double h){
+  display.clearDisplay();
+
+  // display temperature
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("Temperature: ");
+  display.setTextSize(2);
+  display.setCursor(0,10);
+  display.print(t);
+  display.print(" ");
+  display.setTextSize(1);
+  display.cp437(true);
+  display.write(167);
+  display.setTextSize(2);
+  display.print("C");
+  
+  // display humidity
+  display.setTextSize(1);
+  display.setCursor(0, 35);
+  display.print("Humidity: ");
+  display.setTextSize(2);
+  display.setCursor(0, 45);
+  display.print(h);
+  display.print(" %"); 
+  
+  display.display(); 
+}
 
 void setLimits(){
   delay(5000);
-  //poloz w suchym miejscu
-  Serial.println("suche miejsce");
+  stringDisplay("poloz w suchym miejscu");
   delay(8000);
   minSoilContent = 1023 - analogRead(A1);
   
   //wloz czujnik do wody
-  Serial.println("do wody");
+  stringDisplay("wloz czujnik do wody");
   delay(8000);
   maxSoilContent = 1023 - analogRead(A1);
   
   criticalSoilContent = (maxSoilContent - minSoilContent)*0.5 + minSoilContent;
   //czujnik skalibrowany
   delay(2000);
-  Serial.println("jest git");
+  stringDisplay("jest git");
 }
  
 void setup(){
@@ -34,6 +89,11 @@ void setup(){
   Serial.begin(9600);
   pinMode(2,OUTPUT);
   pinMode(A1, INPUT);
+  
+  display.begin(SH1106_SWITCHCAPVCC, 0x3C);
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
 
   setLimits();
 
@@ -44,42 +104,30 @@ void setup(){
 void loop(){
   //Start of Program 
   if(Serial.available()) {
-    var=Serial.read();
+    var = Serial.read();
 
-    switch (var) {
-      case 'A':
-        tone(2, 500);
-        break;
-
-      case 'B':
-        noTone(2);
-        break;
-
-      case 'S':
+    if(var == 'S' || var == 'ST'){
         var = Serial.read();
         int temp = maxSoilContent - minSoilContent;
         criticalSoilContent = temp * (var / 100) + minSoilContent;
-        break;
-
-      case 'T':
+    }else if(var == 'T'){
         var = Serial.read();
         criticalTemp = var;
-        break;
-      default:
-        break;
     }
+
   }
 
     //water content critical
     while(1023 - analogRead(A1) < criticalSoilContent){
-      Serial.println("WATER ME");
+      stringDisplay("WATER ME");
       tone(2, 100);
       delay(1000);
     }
   
     //temperature critical
+    DHT.read11(dht_apin);
     while(DHT.temperature < criticalTemp){
-      Serial.println("TOO COLD");
+      stringDisplay("TOO COLD");
       tone(2, 100);
       delay(1000);
     }
@@ -87,20 +135,20 @@ void loop(){
     //parameters ok
     noTone(2);
 
-    //cur soil Content
-    Serial.print("Current soil Content = "); 
-    int temp = 1023 - analogRead(A1);
-    double curValue = static_cast<double>(temp - minSoilContent) / (maxSoilContent - minSoilContent) ;      
-    Serial.print(curValue * 100); 
-    Serial.println("%");
-    delay(1000);
-    
+
     //cur temperature
     DHT.read11(dht_apin);
-    Serial.print("temperature = ");
-    Serial.print(DHT.temperature); 
-    Serial.println("°C ");
+    double curTemperature = (DHT.temperature);
+  
+
+    //cur soil Content
+    int temp = 1023 - analogRead(A1);
+    double curHumidity = static_cast<double>(temp - minSoilContent) / (maxSoilContent - minSoilContent) ;      
+    parametersDisplay(curTemperature, curHumidity);
+
+    
     delay(1000);
+    
 
 
     
